86f793d36fc2080d2e79c1d8d48e368633175c50
Malcolm Warren <malcolm@sagrona.it>
2016-11-15 14:02:07 +0100

    Reduced the time Home.vue takes to complete

--- imports/ui/Home.vue
+++ imports/ui/Home.vue
@@ -1,6 +1,6 @@
 <template>
         <div style="text-align: center;">
-            <H5>Let's count to 5</H5>
+            <H5>{{ letsCountText }}</H5>
             <h1>{{ count }}</h1>
             <div><quasar-progress :percentage="progress" class="positive"></quasar-progress></div>
             <h2>{{ finished }}</h2>
@@ -10,11 +10,14 @@
 <script>
     import {Session} from 'meteor/session';
 
-    const MAX_COUNT = 5;
+    const MAX_COUNT = 3;
     let myInterval;
     export default {
         meteor: {
             data: {
+                letsCountText() {
+                    return 'Let\'s count to ' + MAX_COUNT;
+                },
                 count() {
                     return Session.get('count');
                 },
@@ -27,15 +30,15 @@
             }
         },
         created() {
-             Session.set('count', 0);
+            Session.set('count', 0);
             let i = 1;
             myInterval =  Meteor.setInterval(function () {
                 Session.set('count', i);
                 i++;
-                if(i > 5){
+                if(i > MAX_COUNT){
                     clearInterval(myInterval);
                 }
-            }, 1000);
+            }, 500);
         },
         destroyed() {
             clearInterval(myInterval);
